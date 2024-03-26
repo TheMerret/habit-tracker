dad453e0c3484ef778535b94ef92db1c6ff67713
TheMerret <merret@bk.ru>
2024-03-26 07:47:03 +0300

    refactor: remove unused filters

--- src/app/(user)/habits/page.tsx
+++ src/app/(user)/habits/page.tsx
@@ -1,13 +1,3 @@
-import { Input } from '@/components/ui/input';
-import {
-  Select,
-  SelectTrigger,
-  SelectValue,
-  SelectContent,
-  SelectGroup,
-  SelectLabel,
-  SelectItem,
-} from '@/components/ui/select';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -36,39 +26,6 @@ export default function Habits() {
           <TabsTrigger value="trash">Корзина</TabsTrigger>
         </TabsList>
         <div className="flex flex-wrap justify-between">
-          <div className="flex gap-7">
-            <Select>
-              <SelectTrigger className="justify-self-start">
-                <SelectValue placeholder="Все периоды" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectLabel>Периоды</SelectLabel>
-                  <SelectItem value="day">День</SelectItem>
-                  <SelectItem value="week">Неделя</SelectItem>
-                  <SelectItem value="month">Месяц</SelectItem>
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-            <Select>
-              <SelectTrigger className="justify-self-start">
-                <SelectValue placeholder="Все категории" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectLabel>Категории</SelectLabel>
-                  <SelectItem value="health">Здоровье</SelectItem>
-                  <SelectItem value="lifestyle">Жизнь</SelectItem>
-                  <SelectItem value="study">Учеба</SelectItem>
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-            <Input
-              type="search"
-              placeholder="Поиск по названию"
-              className="max-w-64 justify-self-start"
-            />
-          </div>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="outline">Добавить</Button>
--- src/app/(user)/habits/templates/page.tsx
+++ src/app/(user)/habits/templates/page.tsx
@@ -1,53 +1,9 @@
-import { Input } from '@/components/ui/input';
-import {
-  Select,
-  SelectTrigger,
-  SelectValue,
-  SelectContent,
-  SelectGroup,
-  SelectLabel,
-  SelectItem,
-} from '@/components/ui/select';
 import { TemplateList } from '@/components/templateList';
 
 export default function Templates() {
   return (
     <div className="flex flex-col gap-y-3">
-      <div className="flex flex-wrap justify-between">
-        <div className="flex gap-7">
-          <Select>
-            <SelectTrigger className="justify-self-start">
-              <SelectValue placeholder="Все периоды" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Периоды</SelectLabel>
-                <SelectItem value="day">День</SelectItem>
-                <SelectItem value="week">Неделя</SelectItem>
-                <SelectItem value="month">Месяц</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-          <Select>
-            <SelectTrigger className="justify-self-start">
-              <SelectValue placeholder="Все категории" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Категории</SelectLabel>
-                <SelectItem value="health">Здоровье</SelectItem>
-                <SelectItem value="lifestyle">Жизнь</SelectItem>
-                <SelectItem value="study">Учеба</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-          <Input
-            type="search"
-            placeholder="Поиск по названию"
-            className="max-w-64 justify-self-start"
-          />
-        </div>
-      </div>
+      <div className="flex flex-wrap justify-between"></div>
       <TemplateList />
     </div>
   );
